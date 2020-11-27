45473d7e2936b120e85217395c60d2a7dcd9f045
Zarin Tasnim <75076461+ZarinTasnimTwinkle@users.noreply.github.com>
2020-11-27 22:49:02 +0600

    Add files via upload

---
+++ CodeChef/servant/servant.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+
+    for(int i=1;i<=t;i++)
+    {
+        cin>>n;
+        if(n<10)
+            cout<<"Thanks for helping Chef!"<<endl;
+        else
+            cout<<"-1"<<endl;
+    }
+}
